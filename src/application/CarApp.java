76e4c861708711c42f30548d1e2e6a3c96955bb3
Donovan Smith <smit7209@stthomas.edu>
2019-04-04 21:41:36 -0500

    GUI Controller Update
    Controller Filled In. Ready for app methods

---
+++ src/application/CarApp.java
@@ -0,0 +1,10 @@
+package application;
+
+public class CarApp {
+	//Instance Variables
+	private int creditScore;
+	private double carPrice;
+	private double downPayment;
+	private double interestRate;
+	private int numMonth;
+}
--- src/application/mainController.java
+++ src/application/mainController.java
@@ -1,5 +1,65 @@
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.*;
+
 public class mainController {
+	//Output Variables
+	@FXML
+	private TextField intPaidTF;
+	@FXML
+	private TextField moPaymentTF;
+	@FXML
+	private TextField totalPaidTF;
+	@FXML
+	private Text creditComment1TF;
+	@FXML
+	private Text creditComment2TF;
+	@FXML
+	private Text creditComment3TF;
 	
+	//Input Variables
+	@FXML
+	private TextField carPriceTF;
+	@FXML
+	private TextField tradeInPaymentTF;
+	@FXML
+	private TextField intRateTF;
+	@FXML
+	private TextField noMonthsTF;
+	@FXML
+	private ChoiceBox<String> creditScoreCB = new ChoiceBox<String>();
+
+	//Main Button
+	@FXML
+	private Button carValueButton;
+	
+	@FXML
+	public void initialize() {
+		//Credit Score Dropdown Creation
+		creditScoreCB.setItems(FXCollections.observableArrayList(
+				"Super prime (781-850)", 
+				"Prime (661-780)", 
+				"Nonprime (601-660)",
+				"Subprime (501-600)", 
+				"Deep subprime (300-500)"
+				));
+		
+		//Main Button
+		carValueButton.setOnAction((event) -> {
+		    //INSERT METHODS TO BE CALLED HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		    moPaymentTF.setText("TEST");
+		    totalPaidTF.setText("TEST2");
+		    intPaidTF.setText("TEST3");
+		    
+		    creditComment1TF.setText("Can you pay in Apples & Milk?");
+		});
+		
+		
+		
+	}	
 }
