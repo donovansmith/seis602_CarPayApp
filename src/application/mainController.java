54ffd2868eb0e379596f6ba5eef4ccbef9bda2eb
Vic England <vicjengland@gmail.com>
2019-04-14 15:26:21 -0500

    Code for 3 Outputs
    Added code for Monthly Payment, Total Paid & Total Interest added.

--- src/application/mainController.java
+++ src/application/mainController.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.text.DecimalFormat;
+
 import javafx.collections.FXCollections;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -52,13 +55,55 @@ public class mainController {
 		//Main Button
 		carValueButton.setOnAction((event) -> {
 		    //INSERT METHODS TO BE CALLED HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		    moPaymentTF.setText("TEST");
-		    totalPaidTF.setText("TEST2");
-		    intPaidTF.setText("TEST3");
+			//convert TextFields to correct variable types
+			double carPrice = Double.parseDouble(carPriceTF.getText());
+			double tradeInPayment = Double.parseDouble(tradeInPaymentTF.getText());
+			double intRate = Double.parseDouble(intRateTF.getText())/1200;
+			int noMonths = Integer.parseInt(noMonthsTF.getText());
+			if (noMonths>84) {
+				noMonths=84;
+				noMonthsTF.setText("84");
+			}
+			
+			//setup new variables
+			DecimalFormat decimal = new DecimalFormat("0.00");
+			double monthlyPay;
+			double principal = carPrice - tradeInPayment;
+			double remainingBalance = principal;
+			double totalPaid;
+			double intPaid;
+			
+			//determine monthlyPay
+			if (intRate != 0) {
+				monthlyPay = ((intRate*principal)/(1-Math.pow((1+intRate),-noMonths)));
+			}
+			else if (principal%noMonths != 0){
+				monthlyPay = principal/noMonths+.005;
+				if (monthlyPay > remainingBalance)
+					monthlyPay = remainingBalance;
+			}
+			else {
+				monthlyPay = principal/noMonths;
+			}
+			
+			//determine total paid
+			if (intRate !=0) {
+				totalPaid = monthlyPay*noMonths;
+			}
+			else {
+				totalPaid = principal;
+			}
+			
+			//determine interest paid
+			intPaid = totalPaid - principal;
+			
+			//output values
+			moPaymentTF.setText(decimal.format(monthlyPay));
+		    totalPaidTF.setText(decimal.format(totalPaid));
+		    intPaidTF.setText(decimal.format(intPaid));
 		    
 		    creditComment1TF.setText("Can you pay in Apples & Milk?");
 		});
-		
 		
 		
 	}	
