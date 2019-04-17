11622906c8b8e46295f30dda14e06adff1f97d27
Vic England <vicjengland@gmail.com>
2019-04-17 10:09:33 -0500

    Changed carPrice
    Changed carPrice (line 61) to principal to include trade in values.

--- src/application/CarApp.java
+++ src/application/CarApp.java
@@ -58,7 +58,7 @@ public class CarApp {
             totalPaid = tempPayment*numMonth;
         }
         else {
-            totalPaid = carPrice;
+            totalPaid = principal;
         }
         
         return totalPaid;
